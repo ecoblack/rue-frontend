49ba28f574899a0da0d1963eb7024aa82af82f59
Igor <igor.popelniuk@gmail.com>
2019-07-01 00:51:02 +0300

    Base commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem 'httparty'
+gem 'nokogiri'
+gem 'byebug'
+gem 'pry', '~> 0.12.2'
+gem 'rubocop-performance'
+gem 'puma'
---
+++ app.rb
@@ -0,0 +1,19 @@
+# class App
+#   def call(env)
+#     req = Rack::Request.new(env)
+#     case req.path_info
+#     when /hello/
+#       [200, {"Content-Type" => "text/html"}, ["Hello World!"]]
+#     when /goodbye/
+#       [500, {"Content-Type" => "text/html"}, ["Goodbye Cruel World!"]]
+#     else
+#       [404, {"Content-Type" => "text/html"}, ["I'm Lost!"]]
+#     end
+#   end
+# end
+
+class App
+  def call(env)
+    [200, {}, ['Hello world']]
+  end
+end
